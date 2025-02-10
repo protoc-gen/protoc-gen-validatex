898018d200b172da95de42c7a24b79fc03a1b148
ifuryst <ifuryst@gmail.com>
2025-02-10 20:55:16 +0800

    fix: Fix the error type.

--- pkg/validatex/error.go
+++ pkg/validatex/error.go
@@ -2,6 +2,6 @@ package validatex
 
 import "github.com/protoc-gen/protoc-gen-go-errors/errors"
 
-func NewError(message string) error {
+func NewError(message string) *errors.Error {
 	return errors.New(ErrCodeInvalidParameters, ErrInvalidParameters, message)
 }
