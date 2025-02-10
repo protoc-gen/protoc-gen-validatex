cf749bb00ee1508d3b4051490f0cc6abf0232cea
ifuryst <ifuryst@gmail.com>
2025-02-10 11:37:52 +0800

    feat: Support extracting value from grpc metadata.

--- go.mod
+++ go.mod
@@ -7,12 +7,13 @@ require (
 	github.com/nicksnyder/go-i18n/v2 v2.5.0
 	github.com/stretchr/testify v1.10.0
 	golang.org/x/text v0.21.0
+	google.golang.org/grpc v1.70.0
 	google.golang.org/protobuf v1.36.3
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/validatex/helper.go
+++ pkg/validatex/helper.go
@@ -1,8 +1,31 @@
 package validatex
 
-import "context"
+import (
+	"context"
+	"google.golang.org/grpc/metadata"
+)
 
 func GetValFromCtx(ctx context.Context, key string) string {
+	s := getFromGrpc(ctx, key)
+	if s != "" {
+		return s
+	}
+
+	return getFromCtx(ctx, key)
+}
+
+func getFromGrpc(ctx context.Context, key string) string {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if ok {
+		vals := md.Get(key)
+		if len(vals) > 0 {
+			return vals[0]
+		}
+	}
+	return ""
+}
+
+func getFromCtx(ctx context.Context, key string) string {
 	val := ctx.Value(key)
 	if val == nil {
 		return ""
